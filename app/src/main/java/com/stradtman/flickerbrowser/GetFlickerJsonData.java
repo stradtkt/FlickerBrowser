9a212f3f0645a3ff1637384e77718bc04c8be95f
Kevin <stradtkt22@gmail.com>
2019-08-22 17:40:21 -0400

    Added GetFlickerJsonData class

---
+++ app/src/main/java/com/stradtman/flickerbrowser/GetFlickerJsonData.java
@@ -0,0 +1,82 @@
+package com.stradtman.flickerbrowser;
+
+import android.net.Uri;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class GetFlickerJsonData implements GetRawData.OnDownloadComplete {
+    private static final String TAG = "GetFlickerJsonData";
+    private List<Photo> mPhotoList = null;
+    private String mBaseURL;
+    private String mLanguage;
+    private boolean mMatchAll;
+
+    private final OnDataAvailable mCallBack;
+    interface OnDataAvailable {
+        void onDataAvailable(List<Photo> data, DownloadStatus status);
+    }
+
+    public GetFlickerJsonData(String baseURL, String language, boolean matchAll, OnDataAvailable callBack) {
+        Log.d(TAG, "GetFlickerJsonData: called");
+        mBaseURL = baseURL;
+        mLanguage = language;
+        mMatchAll = matchAll;
+        mCallBack = callBack;
+    }
+    void executeOnSameThread(String searchCriteria) {
+        Log.d(TAG, "executeOnSameThread: starts");
+        String destinationUri = createUri(searchCriteria, mLanguage, mMatchAll);
+        GetRawData getRawData = new GetRawData(this);
+        getRawData.execute(destinationUri);
+        Log.d(TAG, "executeOnSameThread: ends");
+    }
+    private String createUri(String searchCriteria, String lang, boolean matchAll) {
+        Log.d(TAG, "createUri: starts");
+        return Uri.parse(mBaseURL).buildUpon()
+                .appendQueryParameter("tags", searchCriteria)
+                .appendQueryParameter("tagmode", matchAll ? "ALL" : "ANY")
+                .appendQueryParameter("lang", lang)
+                .appendQueryParameter("nojsoncallback", "1")
+                .build().toString();
+    }
+
+    @Override
+    public void onDownloadComplete(String data, DownloadStatus status) {
+        Log.d(TAG, "onDownloadComplete: Status = " + status);
+        if(status == DownloadStatus.OK) {
+            mPhotoList = new ArrayList<>();
+            try {
+                JSONObject jsonData = new JSONObject(data);
+                JSONArray itemsArray = jsonData.getJSONArray("items");
+                for(int i = 0; i < itemsArray.length(); i++) {
+                    JSONObject jsonPhoto = itemsArray.getJSONObject(i);
+                    String title = jsonPhoto.getString("title");
+                    String author = jsonPhoto.getString("author");
+                    String authorId = jsonPhoto.getString("author_id");
+                    String tags = jsonPhoto.getString("tags");
+                    JSONObject jsonMedia = jsonPhoto.getJSONObject("media");
+                    String photoUrl = jsonMedia.getString("m");
+                    String link = photoUrl.replaceFirst("_m.", "_b.");
+
+                    Photo photoObject = new Photo(title, author, authorId, link, tags, photoUrl);
+                    mPhotoList.add(photoObject);
+                    Log.d(TAG, "onDownloadComplete: " + photoObject.toString());
+                }
+            } catch(JSONException jsone) {
+                jsone.printStackTrace();
+                Log.e(TAG, "onDownloadComplete: Error processing Json data" + jsone.getMessage());
+                status = DownloadStatus.FAILED_OR_EMPTY;
+            }
+        }
+        if(mCallBack != null) {
+            mCallBack.onDataAvailable(mPhotoList, status);
+            Log.d(TAG, "onDownloadComplete: ends");
+        }
+    }
+}

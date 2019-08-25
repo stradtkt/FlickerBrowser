40c7412a37897ce9ac55ce2dd640d60f9380f227
Kevin <stradtkt22@gmail.com>
2019-08-24 22:14:38 -0400

    Updated PhotoDetailActivity

--- app/src/main/java/com/stradtman/flickerbrowser/PhotoDetailActivity.java
+++ app/src/main/java/com/stradtman/flickerbrowser/PhotoDetailActivity.java
@@ -1,11 +1,16 @@
 package com.stradtman.flickerbrowser;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 public class PhotoDetailActivity extends BaseActivity {
 
@@ -15,8 +20,22 @@ public class PhotoDetailActivity extends BaseActivity {
         setContentView(R.layout.activity_photo_detail);
 
         activateToolbar(true);
+        Intent intent = getIntent();
+        Photo photo = (Photo) intent.getSerializableExtra(PHOTO_TRANSFER);
+        if(photo != null) {
+            TextView photoTitle = (TextView) findViewById(R.id.photo_title);
+            photoTitle.setText("Title: " + photo.getTitle());
+            TextView photoTags = (TextView) findViewById(R.id.photo_tags);
+            photoTags.setText("Tags: " + photo.getTags());
+            TextView photoAuthor = (TextView) findViewById(R.id.photo_author);
+            photoAuthor.setText(photo.getAuthor());
+            ImageView photoImage = (ImageView) findViewById(R.id.photo_image);
+            Picasso.with(this).load(photo.getLink())
+                    .error(R.drawable.baseline_image_black_48)
+                    .placeholder(R.drawable.baseline_image_black_48)
+                    .into(photoImage);
+        }
     }
-
 }
 
 
